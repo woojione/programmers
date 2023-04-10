7cc80202bbbed37bf201f89e5ce8a4d37f2b0206
woojione <jione5440@naver.com>
2023-04-10 16:26:28 +0900

    옷가게 할인 받기

---
+++ programmers_2.py
@@ -0,0 +1,33 @@
+# 머쓱이네 옷가게는 10만 원 이상 사면 5%, 30만 원 이상 사면 10%, 50만 원 이상 사면 20%를 할인해줍니다.
+# 구매한 옷의 가격 price가 주어질 때, 지불해야 할 금액을 return 하도록 solution 함수를 완성해보세요.
+
+
+# 제한사항
+# 10 ≤ price ≤ 1,000,000
+# price는 10원 단위로(1의 자리가 0) 주어집니다.
+# 소수점 이하를 버린 정수를 return합니다.
+
+# 입출력 예 #1
+
+# 150,000원에서 5%를 할인한 142,500원을 return 합니다.
+# 입출력 예 #2
+
+# 580,000원에서 20%를 할인한 464,000원을 return 합니다.
+
+def solution(price):
+
+    if 100000<=price<300000:
+        answer=price*0.95
+        return int(answer)
+    elif 300000<=price<500000:
+        answer=price*0.9
+        return int(answer)
+    elif price>=500000:
+        answer=price*0.8
+        return int(answer)
+    else:
+        return int(price)
+
+price=int(input("가격을 입력하세요:"))
+print("가격: ",price)
+print(solution(price))
